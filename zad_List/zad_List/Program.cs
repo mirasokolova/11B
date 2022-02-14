cebb0c56692c49e697ac99d0cf6f9ae0cbbc613b
mirasokolova <mira.rosenova@gmail.com>
2022-02-14 08:56:38 +0200

    List

---
+++ zad_List/zad_List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zad_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
