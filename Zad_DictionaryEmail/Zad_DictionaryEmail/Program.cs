c78a299c96b50d48f96375ebb079eb903011b3b0
mirasokolova <mira.rosenova@gmail.com>
2022-02-28 10:38:10 +0200

    Dictionary

---
+++ Zad_DictionaryEmail/Zad_DictionaryEmail/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zad_DictionaryEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emailList = new Dictionary<string, string>();
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+
+                if (input[0] == "Stop")
+                {
+                    foreach (var item in emailList )
+                    {
+
+                        Console.WriteLine($"{ item.Key} –> {item.Value}");
+                    }
+                    break;
+                }
+
+                if (input[0] == "Add")
+
+                {
+                    string name = input[1];
+                    string email = input[2];
+                    if (!emailList.ContainsKey(name))
+                    {
+                        emailList.Add(name, email);
+                    }
+                    else
+                    {
+                        emailList[name] = email;
+                    }
+                }
+                if (input[0] == "Sent")
+                {
+                    string name = input[1];
+                    
+                    if (emailList.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name}->{emailList[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exists.");
+
+                    }
+                }
+                
+                
+
+            }
+            
+        }
+    }
+}
---
+++ Zad_Gold/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> products = new Dictionary<string, int>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+
+                if (input == "Stop")
+                {
+                    foreach(var product in products)
+                    {
+                        Console.WriteLine($"{product.Key} -> {product.Key}");
+                    }
+                    break;
+                }
+                string productName = input;
+                int quantity = int.Parse(Console.ReadLine());
+                if (!products.ContainsKey(productName))
+                {
+                    products.Add(productName, quantity);
+                }
+                else
+                {
+                    products[productName] += quantity;
+                }
+            
+            }
+        }
+    }
+}
